1a4b0fae2aa0af6f873a08b6655303a302ad1d4e
Angel Ricardo Gonzalez Soto <angelricardoglezsoto@gmail.com>
2026-06-10 11:38:47 -0600

    Implementar funciones en NASM

--- build.bat
+++ build.bat
@@ -1,17 +1,9 @@
 @echo off
-
-echo [1/3] Ensamblando ASM (Modo 32-bits)...
-nasm -f win32 src/rutinas_asm.asm -o src/rutinas_asm.obj
-
-echo [2/3] Compilando C y enlazando modulos...
+nasm -f win64 src/rutinas_asm.asm -o src/rutinas_asm.obj
 gcc src/main.c src/logica_juego.c src/rutinas_asm.obj -o BitQuest.exe
-
-echo [3/3] Ejecutando juego...
 if exist BitQuest.exe (
     BitQuest.exe
 ) else (
-    echo.
     echo [ERROR] No se pudo crear el archivo ejecutable.
 )
-
 pause
--- src/rutinas_asm.asm
+++ src/rutinas_asm.asm
@@ -1,13 +1,78 @@
-global _es_colision
+global contar_caracteres
+global validar_movimiento
+global calcular_puntaje
+global detectar_objeto
+global contar_celdas_libres
 
 section .text
-_es_colision:
-    mov cl, [esp + 4]  
-    
-    cmp cl, '#'        
-    je .pared         
-    mov eax, 0        
+
+contar_caracteres:
+    xor rax, rax
+    xor r10, r10
+.bucle_cc:
+    cmp r10, rdx
+    jge .fin_cc
+    mov r11b, [rcx + r10]
+    cmp r11b, r8b
+    jne .sig_cc
+    inc rax
+.sig_cc:
+    inc r10
+    jmp .bucle_cc
+.fin_cc:
     ret
-.pared:
-    mov eax, 1         ; Retornamos 1 (colisión)
+
+validar_movimiento:
+    mov rax, r8
+    imul rax, rdx
+    add rax, r9
+    mov r10b, [rcx + rax]
+    cmp r10b, 35
+    je .bloqueado
+    mov rax, 1
+    ret
+.bloqueado:
+    mov rax, 0
+    ret
+
+calcular_puntaje:
+    mov rax, rcx
+    imul rax, 100
+    imul rdx, 2
+    sub rax, rdx
+    imul r8, 500
+    add rax, r8
+    cmp rax, 0
+    jge .fin_cp
+    xor rax, rax
+.fin_cp:
+    ret
+
+detectar_objeto:
+    mov rax, r8
+    imul rax, rdx
+    add rax, r9
+    mov r10b, [rcx + rax]
+    cmp r10b, sil
+    je .encontrado
+    mov rax, 0
+    ret
+.encontrado:
+    mov rax, 1
+    ret
+
+contar_celdas_libres:
+    xor rax, rax
+    xor r10, r10
+.bucle_cl:
+    cmp r10, rdx
+    jge .fin_cl
+    mov r11b, [rcx + r10]
+    cmp r11b, 46
+    jne .sig_cl
+    inc rax
+.sig_cl:
+    inc r10
+    jmp .bucle_cl
+.fin_cl:
     ret
